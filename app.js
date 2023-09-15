17ff7dcc9cc47ff8d1fefc780ca34a1f0494c9df
mgaungthukomm <145087776+mgaungthukomm@users.noreply.github.com>
2023-09-15 10:44:06 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+////Do Not Edit Below This
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyDmJve9fp8Mv3XgkKmGfv-jz9JmVbUIA-M",
+    authDomain: "kaknoport.firebaseapp.com",
+    databaseURL: "kaknoport-default-rtdb.firebaseio.com",
+    projectId: "kaknoport",
+    storageBucket: "kaknoport.appspot.com",
+    messagingSenderId: "298623184983",
+    appId: "1:298623184983:android:ce167a47b0f7cffe513312"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
